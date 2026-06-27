c15140667f9f04759a88080d2463fdc749925c1a
bonk <cjreastmann@gmail.com>
2026-06-27 11:05:37 -0400

    COmplete DatabaseConnection test

--- MediaTracker/src/main/java/media/persistence/DatabaseConnection.java
+++ MediaTracker/src/main/java/media/persistence/DatabaseConnection.java
@@ -4,6 +4,8 @@ import java.sql.*;
 
 public class DatabaseConnection {
 	public static Connection getConnection() throws SQLException{
-		return DriverManager.getConnection("jdbc:postgresql://localhost:5432/media_tracker", "root", "");
+		return DriverManager.getConnection("jdbc:postgresql://localhost:5432/test_db", "root", "root");
 	}
 }
+
+//make sure container has volume in-case of removable
