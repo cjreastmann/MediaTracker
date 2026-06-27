315e9e61570c051347e40f3d18e443105b4cb924
bonk <cjreastmann@gmail.com>
2026-06-27 00:07:29 -0400

    Added Tag model

---
+++ MediaTracker/src/main/java/media/model/Tag.java
@@ -0,0 +1,32 @@
+package media.model;
+
+public class Tag {
+	private int tagId;
+	private String tagName;
+	
+	//existing tag, known id and name
+	public Tag(int tagId, String name) {
+		this.tagId = tagId;
+		this.tagName = name;
+	}
+	
+	//tag creation, unknown id, name required
+	public Tag(String name) {
+		this(0, name);
+	}
+	
+	//getters
+	public int getTagId() {
+		return tagId;
+	}
+	
+	public String getTagName() {
+		return tagName;
+	}
+	
+	//setters
+	public void setTagName(String t) {
+		tagName = t;
+	}
+	
+}
