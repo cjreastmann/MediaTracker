2125fcd877e0e5d8677ea2627cf38f8157e492c1
bonk <cjreastmann@gmail.com>
2026-06-26 23:19:11 -0400

    Add Category model

--- MediaTracker/src/main/java/media/model/Category.java
+++ MediaTracker/src/main/java/media/model/Category.java
@@ -1,5 +1,23 @@
 package media.model;
 
 public class Category {
+	private int categoryId;
+	private String name;
+	
+	public Category(int categoryId, String name) {
+		this.categoryId = categoryId;
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getCategoryId() {
+		return categoryId;
+	}
+	
+	//maybe add setter for name later
+	
 
 }
