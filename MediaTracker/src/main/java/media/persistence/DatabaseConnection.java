7733223711726146418596314f57a6c161453d7c
bonk <cjreastmann@gmail.com>
2026-07-08 22:03:16 -0400

    Added db.properties to .gitignore

--- MediaTracker/src/main/java/media/persistence/DatabaseConnection.java
+++ MediaTracker/src/main/java/media/persistence/DatabaseConnection.java
@@ -1,11 +1,25 @@
 package media.persistence;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
 public class DatabaseConnection {
-	public static Connection getConnection() throws SQLException{
-		return DriverManager.getConnection("jdbc:postgresql://localhost:5432/test_db", "root", "root");
-	}
+    public static Connection getConnection() throws SQLException {
+        try {
+            Properties props = new Properties();
+            InputStream input = DatabaseConnection.class
+                .getClassLoader()
+                .getResourceAsStream("db.properties");
+            props.load(input);
+            String url = props.getProperty("db.url");
+            String username = props.getProperty("db.username");
+            String password = props.getProperty("db.password");
+            return DriverManager.getConnection(url, username, password);
+        } catch (IOException e) {
+            throw new SQLException("Failed to load database properties", e);
+        }
+    }
 }
-
 //make sure container has volume in-case of removable
